9fe3ecd93cf50daaee38ba1a4f88970b235a5ea0
enter255 <enter255@naver.com>
2019-05-24 03:19:04 +0900

    we have donegit add .git add .git add .git add . HW02

--- catkin_ws/src/knu_ros_lecture/src/turtle_auto_move.cpp
+++ catkin_ws/src/knu_ros_lecture/src/turtle_auto_move.cpp
@@ -40,9 +40,26 @@ using namespace cv;
 boost::mutex mutex[2];
 nav_msgs::Odometry g_odom;
 sensor_msgs::LaserScan g_scan;
-
+vector<Vec3d> wayPoints;
 float pre_dAngleTurned;
-double x[20], y[20];	// robot path
+double x[20], y[20]; // robot path
+
+
+void odomMsgCallback(const nav_msgs::Odometry &msg);
+void convertOdom2XYZ(nav_msgs::Odometry &odom, Vec3d &curPos, Vec3d &curRot);
+bool doTranslation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation, double dTranslation, double dTranslationSpeed, Vec3d &curPos, Vec3d &initPos, Vec3d &curRot, bool &flag, vector<Vec3d> &route, Mat &display);
+void transformScanXY(vector<Vec3d> &laserScanXY, double x, double y, double theta);
+tf::Transform getCurrentTransformation(void);
+tf::Transform getInitialTransformation(void);
+bool doRotation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation, double dRotation, double dRotationSpeed);
+void displayRoute(Mat &display, vector<Vec3d> &route, Vec3d &curRot);
+void convertScan2XYZs(sensor_msgs::LaserScan& lrfScan, vector<Vec3d> &XYZs);
+void initGrid(Mat &display, int nImageSize);
+void scanMsgCallback(const sensor_msgs::LaserScan& msg);
+void drawLRFScan(Mat &display, vector<Vec3d> &laserScanXY, double dMaxDist);
+void drawOdom(Mat &display, vector<Vec3d> &route, int rSize, double dMaxDist);
+void drawTrajectory(Mat &display, vector<Vec3d> &trajectory, Vec3d &curPos, double dMaxDist);
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -54,16 +71,18 @@ void odomMsgCallback(const nav_msgs::Odometry &msg)
 		g_odom = msg;
 	} mutex[0].unlock();
 
-	// print g_odom here!
-	//std::cout << "odom_info: [position: " << g_odom.pose.pose.position.x << ", " 
-	//		<< g_odom.pose.pose.position.y << ", " << g_odom.pose.pose.position.z << "]\n";
-
-	//std::cout << "           [orientation: " << g_odom.pose.pose.orientation.x << ", " 
-	//					<< g_odom.pose.pose.orientation.y << ", " 
-	//					<< g_odom.pose.pose.orientation.z << ", " 
-	//					<< g_odom.pose.pose.orientation.w << "]\n\n";
-
 }
+
+
+	void
+scanMsgCallback(const sensor_msgs::LaserScan& msg)
+{
+	// receive a '/odom' message with the mutex
+	mutex[1].lock(); {
+		g_scan = msg;
+	} mutex[1].unlock();
+}
+
 
 ///////////////////////////////////////////////////////////
 /*** odom으로부터 받은 좌표를 평면좌표로 변환하는 function ***/
@@ -78,6 +97,95 @@ void convertOdom2XYZ(nav_msgs::Odometry &odom, Vec3d &curPos, Vec3d &curRot)
 	tf::Quaternion rotationQuat = tf::Quaternion(odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w);
 	tf::Matrix3x3(rotationQuat).getEulerYPR(curRot[2], curRot[1], curRot[0]);
 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Move
+	bool
+doTranslation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation, double dTranslation, double dTranslationSpeed, Vec3d &curPos, Vec3d &initPos, Vec3d &curRot, bool &flag, vector<Vec3d> &route, Mat &display)
+{
+	//the command will be to go forward at 'translationSpeed' m/s
+	geometry_msgs::Twist baseCmd;
+	nav_msgs::Odometry odom;
+
+	if(dTranslation < 0) {
+		baseCmd.linear.x = -dTranslationSpeed;
+	} else {
+		baseCmd.linear.x = dTranslationSpeed;
+	}
+
+	baseCmd.linear.y = 0;
+	baseCmd.angular.z = 0;
+
+	// Get odometry messages of current position while moving
+	bool bDone = false;
+	ros::Rate loopRate(1000.0);
+
+	while(ros::ok() && !bDone) {
+		// Get callback messages
+		ros::spinOnce();
+
+		// get current transformation
+		tf::Transform currentTransformation = getCurrentTransformation();
+
+		/* odom */
+		mutex[0].lock(); {
+			odom = g_odom;
+		} mutex[0].unlock();
+
+		// odom으로부터 이동정보 획득
+		convertOdom2XYZ(odom, curPos, curRot);
+		// route에 저장
+		double x = curPos[0];
+		double y = curPos[1];
+		if(x!=0 && y!=0) {
+			route.push_back(Vec3d(x,y));
+			if(flag == false) {
+				initPos[0] = x; initPos[1] = y;
+				flag = true;				
+			}
+		}
+		printf("current position : %.3lf , %.3lf\n", curPos[0], curPos[1]);
+
+
+
+		/* 3. display opencv window  */
+		displayRoute(display, route, curRot);
+
+
+		/* Check termination condition */
+		//see how far we've traveled
+		tf::Transform relativeTransformation = initialTransformation.inverse() * currentTransformation ;
+		double dDistMoved = relativeTransformation.getOrigin().length();
+
+		if(fabs(dDistMoved) >= fabs(dTranslation)) {
+			bDone = true;
+			wayPoints.push_back(Vec3d(x,y,0));// save real destination point
+			break;
+		} else {
+			//send the drive command
+			pubTeleop.publish(baseCmd);
+
+			// sleep!
+			loopRate.sleep();
+		}
+
+		
+
+	}
+
+	//  Initialization
+	baseCmd.linear.x = 0.0;
+	baseCmd.angular.z = 0.0;
+	pubTeleop.publish(baseCmd);
+
+	ros::Duration(0.5, 0).sleep();
+	
+	// 사용자의 키보드 입력을 받음!
+	waitKey(30);
+
+
+	return bDone;
 }
 
 //3차원 point인 laserScanXY를 world coordinate로 변환
@@ -165,10 +273,8 @@ bool doRotation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation,
 	ros::Rate loopRate(1000.0);
 
 
-
 	while(ros::ok() && !bDone) {
 		// Get callback messages
-
 		ros::spinOnce();
 
 		// get current transformation
@@ -190,10 +296,10 @@ bool doRotation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation,
 			pre_dAngleTurned = dAngleTurned;
 			//send the drive command
 			pubTeleop.publish(baseCmd);
-			//printf("doRotation dAngleTurned : %lf######\n", dAngleTurned);/////
 			// sleep!
 			loopRate.sleep();
 		}
+
 	}
 
 	// Initialization
@@ -204,83 +310,49 @@ bool doRotation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation,
 	return bDone;
 }
 
+////////////////////
+
+void
+displayRoute(Mat &display, vector<Vec3d> &route, Vec3d &curRot) {
+	sensor_msgs::LaserScan scan;
+	vector<Vec3d> laserScanXY;
+	const double dGridMaxDist = 4.5;
+	int routeSize = route.size();
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Move
-	bool
-doTranslation(ros::Publisher &pubTeleop, tf::Transform &initialTransformation, double dTranslation, double dTranslationSpeed, Vec3d &curPos, Vec3d &initPos, Vec3d &curRot, bool flag, vector<Vec3d> &route)
-{
-	//the command will be to go forward at 'translationSpeed' m/s
-	geometry_msgs::Twist baseCmd;
-	nav_msgs::Odometry odom;
-
-	if(dTranslation < 0) {
-		baseCmd.linear.x = -dTranslationSpeed;
-	} else {
-		baseCmd.linear.x = dTranslationSpeed;
-	}
-
-	baseCmd.linear.y = 0;
-	baseCmd.angular.z = 0;
-
-	// Get odometry messages of current position while moving
-	bool bDone = false;
-	ros::Rate loopRate(1000.0);
-
-	while(ros::ok() && !bDone) {
-		// Get callback messages
-		ros::spinOnce();
-
-		// get current transformation
-		tf::Transform currentTransformation = getCurrentTransformation();
-
-		/* odom */
-		mutex[0].lock(); {
-			odom = g_odom;
-		} mutex[0].unlock();
+	ros::spinOnce();
+	// callback 함수 call!
+	// receive the global '/scan' message with the mutex
+	mutex[1].lock(); {
+		scan = g_scan;
+	} mutex[1].unlock();
+	// scan으로부터 Cartesian X-Y scan 획득
+	convertScan2XYZs(scan, laserScanXY);
+	// convert laserScan into 2D coordinate
+	Vec3d curPos = route[routeSize-1];
+	transformScanXY(laserScanXY, curPos[0], curPos[1], curRot[2]);
 
 
-		/* Check termination condition */
-		//see how far we've traveled
-		tf::Transform relativeTransformation = initialTransformation.inverse() * currentTransformation ;
-		double dDistMoved = relativeTransformation.getOrigin().length();
+	// 현재 상황을 draw할 display 이미지를 생성
+	initGrid(display, MAP_SIZE);
+	drawLRFScan(display, laserScanXY, dGridMaxDist);
+	//drawOdom(display, route, routeSize, dGridMaxDist);
+	drawTrajectory(display, route, curPos, dGridMaxDist);
 
-		if(fabs(dDistMoved) >= fabs(dTranslation)) {
-			bDone = true;
-
-			// odom으로부터 이동정보 획득
-			convertOdom2XYZ(odom, curPos, curRot);
-			// route에 저장
-			double x = curPos[0];
-			double y = curPos[1];
-			if(x!=0 && y!=0) {
-				route.push_back(Vec3d(x,y));
-				if(flag == false) {
-					initPos[0] = x; initPos[1] = y;
-					flag = true;
-					printf("***************** init : %lf , %lf *****************\n", initPos[0], initPos[1]);
-				}
-			}
-			printf("******* cur : %lf , %lf *******\n", curPos[0], curPos[1]);
 	
-			break;
-		} else {
-			//send the drive command
-			pubTeleop.publish(baseCmd);
+	// 2D 영상좌표계에서 top-view 방식의 3차원 월드좌표계로 변환
+	transpose(display, display); // X-Y축 교환
+	flip(display, display, 0); // 수평방향 반전
+	flip(display, display, 1); // 수직방향 반전
+	imshow("KNU ROS Lecture >> turtle_kinect_lrf_view", display);
+	// image 출력
 
-			// sleep!
-			loopRate.sleep();
-		}
-	}
+	// 사용자의 키보드 입력을 받음!
+	waitKey(30);
 
-	//  Initialization
-	baseCmd.linear.x = 0.0;
-	baseCmd.angular.z = 0.0;
-	pubTeleop.publish(baseCmd);
-
-	return bDone;
 }
+
+
 
 /////////////////////////********lidar_scan*********///////////////////////////////
 
@@ -314,22 +386,8 @@ initGrid(Mat &display, int nImageSize)
 	const int nAxisSize = nImageSize/16;
 	const Vec2i imageCenterCooord = Vec2i(nImageHalfSize, nImageHalfSize);
 	display = Mat::zeros(nImageSize, nImageSize, CV_8UC3);
-	//x축
-	//      line(display, Point(imageCenterCooord[0], imageCenterCooord[1]), Point(imageCenterCooord[0]+nAxisSize, imageCenterCooord[1]), Scalar(0, 0, 255), 2);
-	//        line(display, Point(curPos[0], curPos[1]), Point(curPos[0]+nAxisSize, curPos[1]), Scalar(0, 0, 255), 10);
-	//y축   
-	//        line(display, Point(curPos[0], curPos[1]), Point(curPos[0], curPos[1]+nAxisSize), Scalar(0, 255, 0), 10);
 }
 
-
-	void
-scanMsgCallback(const sensor_msgs::LaserScan& msg)
-{
-	// receive a '/odom' message with the mutex
-	mutex[1].lock(); {
-		g_scan = msg;
-	} mutex[1].unlock();
-}
 
 	void
 drawLRFScan(Mat &display, vector<Vec3d> &laserScanXY, double dMaxDist)
@@ -370,46 +428,30 @@ void drawOdom(Mat &display, vector<Vec3d> &route, int rSize, double dMaxDist)
 
 }
 
-////////////////////
-
 void
-displayRoute(Mat &display, vector<Vec3d> &route, Vec3d &curRot) {
-	sensor_msgs::LaserScan scan;
-	vector<Vec3d> laserScanXY;
-	const double dGridMaxDist = 4.5;
-	int routeSize = route.size();
+drawTrajectory(Mat &display, vector<Vec3d> &trajectory, Vec3d &curPos, double dMaxDist)
+{
+	Vec2i imageHalfSize = Vec2i(display.cols/2, display.rows/2);
+	int nSize = (int) trajectory.size();
 
+	for(int i=0; i<nSize; i++) {
+		double x = imageHalfSize[0] + cvRound((trajectory[i][0]/dMaxDist)*imageHalfSize[0]);
+		double y = imageHalfSize[1] + cvRound((trajectory[i][1]/dMaxDist)*imageHalfSize[1]);
 
-	ros::spinOnce();
-	// callback 함수 call!
-	// receive the global '/scan' message with the mutex
-	mutex[1].lock(); {
-		scan = g_scan;
-	} mutex[1].unlock();
-	// scan으로부터 Cartesian X-Y scan 획득
-	convertScan2XYZs(scan, laserScanXY);
-	// convert laserScan into 2D coordinate
-	Vec3d curPos = route[routeSize-1];
-	transformScanXY(laserScanXY, curPos[0], curPos[1], curRot[2]);
+		if(x >= 0 && x < display.cols && y >= 0 && y < display.rows) {
+			circle(display, Point(x, y), 1, CV_RGB(255, 255, 0), 1, CV_AA);
+		}
+	}
 
-
-	// 현재 상황을 draw할 display 이미지를 생성
-	initGrid(display, MAP_SIZE);
-	drawLRFScan(display, laserScanXY, dGridMaxDist);
-	drawOdom(display, route, routeSize, dGridMaxDist);
-
-	// 2D 영상좌표계에서 top-view 방식의 3차원 월드좌표계로 변환
-	//transpose(display, display); // X-Y축 교환
-	//flip(display, display, 0); // 수평방향 반전
-	//flip(display, display, 1); // 수직방향 반전
-	imshow("KNU ROS Lecture >> turtle_kinect_lrf_view", display);
-	// image 출력
-
-
-	// 사용자의 키보드 입력을 받음!
-	waitKey(30);
-
+	for(int i=0; i<wayPoints.size(); i++) {
+		double x = imageHalfSize[0] + cvRound((wayPoints[i][0]/dMaxDist)*imageHalfSize[0]);
+		double y = imageHalfSize[1] + cvRound((wayPoints[i][1]/dMaxDist)*imageHalfSize[1]);
+		circle(display,Point(x,y),5,CV_RGB(255,0,0),-1,CV_AA);
+	}
 }
+
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
@@ -435,46 +477,35 @@ int main(int argc, char **argv)
 	fscanf(fp, "%d", &num);
 	for(int i = 0; i < num; i++){
 		fscanf(fp, "%lf %lf", &x[i], &y[i]);
-		printf("%d: (%.2lf , %.2lf)\n", (i+1), x[i],y[i]);
 	}
 
 	fclose(fp);
 
-	double angle_vel = 0.75;
+	double angle_vel = 0.15;
 	double linear_vel = 0.25;
 	double prev_angle = 0;
 
 
+
 	//move 10 times
-	for(int i = 0; i < num; i++){
+	for(int i = 0; i < num; i++)
+	{
 
 		/* 2. Get initial position */
 		tf::Transform nextTransformation = getInitialTransformation();
 
-		printf("\n## start point: (%.2lf, %.2lf)\n", nextTransformation.getOrigin().getX(),nextTransformation.getOrigin().getY());
-		printf("\n## goal point: (%.2lf, %.2lf)\n", x[i], y[i]);
-
-		// 2-1. Sleep for 1 sec
-		ros::Duration(0.5, 0).sleep();
-
 		// 2-3. Rotate robot.
 		double angle = atan2(y[i]-nextTransformation.getOrigin().getY(), x[i]-nextTransformation.getOrigin().getX());
-
-		printf("## current angle: %lf\n", toDegree(angle));
-
-
 		doRotation(pub, nextTransformation, angle-prev_angle, angle_vel);
-		printf("## command angle: %lf\n", toDegree(angle-prev_angle));
 
 		// 2-4. Move robot forward.
 		double distance = sqrt((pow(x[i]-nextTransformation.getOrigin().getX(), 2.0)) + (pow(y[i]-nextTransformation.getOrigin().getY(), 2.0)));
-		doTranslation(pub, nextTransformation, distance, linear_vel, curPos, prePos, curRot, flag, route);
+
+		doTranslation(pub, nextTransformation, distance, linear_vel, curPos, prePos, curRot, flag, route, display);
 		prev_angle = angle;
 
-
-		/* 3. display opencv window  */
-		displayRoute(display, route, curRot);
-
 	}
+
+
 	return 0;
 }
